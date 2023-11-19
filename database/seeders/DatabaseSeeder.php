3ce47e403777de4414ea3e0557c6a5b13413d533
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-11-19 01:33:03 +0100

    called customer seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,9 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
+            $this->call([
+                CustomerSeeder::class
+            ]);
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
